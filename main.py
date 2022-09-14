a21f773d9b73c26f53f7188d939ab645588a5b6c
Marit Vons <74767400+maritvons@users.noreply.github.com>
2022-09-14 09:29:05 +0000

    'tegenstander' iets slimmer gemaakt

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import random
-from bke import EvaluationAgent, start
 
-class MyRandomAgent(EvaluationAgent):
+from bke import EvaluationAgent, start, can_win
+
+class MijnSpeler(EvaluationAgent):
   def evaluate(self, board, my_symbol, opponent_symbol):
-    return random.randint(1, 500)
+    getal = 1
+    if can_win(board, opponent_symbol):
+      getal = getal - 1000
+    return getal
 
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent, player_x=my_random_agent)
+mijn_speler = MijnSpeler()
+start(player_o=mijn_speler)
