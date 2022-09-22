ddaa2f17243751664d8c93bcae5dda3abeba9a5c
Marit Vons <74767400+maritvons@users.noreply.github.com>
2022-09-22 10:50:34 +0000

    begin startmenu

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 import random
- 
-from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
- 
- 
+from bke import MLAgent, EvaluationAgent, is_winner, opponent, RandomAgent, train_and_plot, load, start 
+
 class MyAgent(MLAgent):
-    def evaluate(self, board):
-        if is_winner(board, self.symbol):
-            reward = 1
-        elif is_winner(board, opponent[self.symbol]):
-            reward = -1
-        else:
-            reward = 0
-        return reward
-    
-    
-random.seed(1)
- 
+  def evaluate(self, board):
+    if is_winner(board, self.symbol):
+      reward = 1
+    elif is_winner(board, opponent[self.symbol]):
+      reward = -1
+    else:
+      reward = 0
+    return reward
+   
 my_agent = MyAgent()
-random_agent = RandomAgent()
- 
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
+my_agent = load("MyAgent_3000")
+   
+my_agent.learning = False
+
+
+#start menu
+print("1. een speler (slim) \n2. een speler (dom) \n3. twee spelers \n4. tegenstander trainen \n5. validatie grafiek")
+
+x=input("keuze:")
+if x=="1":
+  print("slimme speler")
+  start(player_o=my_agent)
+  
+elif x=="2":
+  print("domme speler")
+  r=2
+elif x=="3":
+  print("twee spelers")
+  r = 3
+elif x=="4":
+  print("tegenstander trainen")
+  r = 4
+elif x=="5":
+  print("validatie grafiek")
+  r = 5
+else:
+  print("incorrecte input")
+
--- train_save.py
+++ train_save.py
@@ -17,3 +17,30 @@ my_agent = MyAgent()
 train(my_agent, 3000)
  
 save(my_agent, 'MyAgent_3000')
+
+
+
+
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+    
+random.seed(1)
+ 
+my_agent = MyAgent()
+my_agent = MyAgent(alpha=1.0, epsilon=1.0)
+random_agent = RandomAgent()
+ 
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=100,
+    validations=1000)
