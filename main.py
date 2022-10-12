380398d766f566b386fff9984dce99fd0e61414f
Marit Vons <74767400+maritvons@users.noreply.github.com>
2022-10-12 10:48:33 +0000

    code netjes gemaakt & playagain

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import random
-from bke import MLAgent, EvaluationAgent, is_winner, opponent, RandomAgent, train_and_plot, load, validate, plot_validation, start 
+from bke import MLAgent, EvaluationAgent, is_winner, opponent, RandomAgent, load, start
 
+#makkelijk
+class MyRandomAgent(EvaluationAgent):
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    return random.randint(1,500)
+
+#moeilijk
+class MyAgent(MLAgent):
+  def evaluate(self, board):
+    if is_winner(board, self.symbol):
+      reward = 1
+    elif is_winner(board, opponent[self.symbol]):
+      reward = -1
+    else:
+      reward = 0
+    return reward         
+
+#train&plot
+class MyAgent(MLAgent):
+  def evaluate(self, board):
+    if is_winner(board, self.symbol):
+      reward = 1
+    elif is_winner(board, opponent[self.symbol]):
+      reward = -1
+    else:
+      reward = 0
+    return reward
+
+my_random_agent = MyRandomAgent()
+
+my_agent = MyAgent(alpha=0.9, epsilon=0.3)
+my_agent = load("MyAgent_3000")      
+my_agent.learning = True
+
+my_trainagent = MyAgent(alpha=1.0, epsilon=0.1)
+random_agent = RandomAgent()
+    
 #start menu
 print("Welkom bij boter, kaas en eieren!")
 
@@ -13,62 +49,37 @@ def games():
     y=input("keuze:")
     if y=="1":
       print("makkelijk")
-  
-      class MyRandomAgent(EvaluationAgent):
-        def evaluate(self, board, my_symbol, opponent_symbol):
-          return random.randint(1,500)
-    
-      my_random_agent = MyRandomAgent(alpha=0.1, epsilon=1.0)
+      
       start(player_o=my_random_agent)
+
+      playagain()
       
     elif y=="2":
       print("moeilijk")
-      
-      class MyAgent(MLAgent):
-        def evaluate(self, board):
-          if is_winner(board, self.symbol):
-            reward = 1
-          elif is_winner(board, opponent[self.symbol]):
-            reward = -1
-          else:
-            reward = 0
-          return reward
-         
-      my_agent = MyAgent(alpha=0.9, epsilon=0.3)
-      my_agent = load("MyAgent_3000")
-       
-      my_agent.learning = True
+
       start(player_o=my_agent)
+
+      playagain()
     
   elif x=="2":
     print("twee spelers")
   
     start()
-    
+
+    playagain()
   elif x=="3":
     print("trainen en valideren /n")
-  
-    class MyAgent(MLAgent):
-      def evaluate(self, board):
-          if is_winner(board, self.symbol):
-              reward = 1
-          elif is_winner(board, opponent[self.symbol]):
-              reward = -1
-          else:
-              reward = 0
-          return reward
    
     random.seed(1)
-   
-    my_agent = MyAgent(alpha=1.0, epsilon=0.1)
-    random_agent = RandomAgent()
-     
+  
     train_and_plot(
-        agent=my_agent,
+        agent=my_trainagent,
         validation_agent=random_agent,
-        iterations=100,
+        iterations=50,
         trainings=500,
         validations=1000)
+
+    playagain()
 
   else:
     print("Uw input is incorrect \n")
@@ -76,12 +87,13 @@ def games():
 
 
 
+def playagain():
+  print("Wilt u opnieuw beginnen?")
+  y=input()
+  if y=="ja" or y=="Ja" or y=="JA": 
+    print("\n")
+    games() 
+  elif y=="nee" or y=="Nee" or y=="NEE":
+    print("Tot ziens!")
+    
 games()
-
-print("Wilt u opnieuw beginnen?")
-y=input()
-if y=="ja" or y=="Ja" or y=="JA": 
-  print("\n")
-  games() 
-elif y=="nee" or y=="Nee" or y=="NEE":
-  print("Tot ziens!")
